94c1b860ad365057abb02a8eae61d59c557e7a89
foreverbell <dql.foreverbell@gmail.com>
2014-12-26 16:30:16 +0800

    100 & diophantine

---
+++ 100/100.hs
@@ -0,0 +1,7 @@
+
+possiblePairs :: [(Integer, Integer)]
+possiblePairs = (1,1) : map foo possiblePairs where
+    foo (a,b) = (3*a+4*b-3, 2*a+3*b-2)
+
+main = print $ snd $ head $ dropWhile ((<= bound) . fst) $ possiblePairs where
+    bound = round $ 10**12
--- 66/66.hs
+++ 66/66.hs
@@ -1,26 +1,21 @@
 import Data.List (maximumBy)
 import Data.Function (on)
 
-diophantine n root a b = x : (diophantine n root a' b')
-    where
+isqrt = floor . sqrt . fromIntegral
+isSquared x = (isqrt x) ^ 2 == x
+
+diophantine d = filter (\(x,y) -> x^2-d*y^2 == 1) convergent where
+    root = isqrt d
+    cf = helper d root 1 root
+    convergent = tail $ zip xs ys
+    xs = 1 : root : (zipWith3 (\a b c -> a * b + c) cf (tail xs) xs)
+    ys = 0 : 1 : (zipWith3 (\a b c -> a * b + c) cf (tail ys) ys)
+    helper n root a b = x : (helper n root a' b') where
         a' = (n - b * b) `div` a
         x = (root + b) `div` a'
         b' = x * a' - b
 
-solve d = fst $ head $ dropWhile (\(x,y) -> x^2-d*y^2 /= 1) convergent
-    where
-        cf = diophantine d root 1 root
-        convergent = tail $ zip xs ys
-        xs = 1 : root : (zipWith3 (\a b c -> a * b + c) cf (tail xs) xs)
-        ys = 0 : 1 : (zipWith3 (\a b c -> a * b + c) cf (tail ys) ys)
-        isqrt = floor . sqrt . fromIntegral
-        root = isqrt d
-
-main = print $ maximumBy (compare `on` solve) nonSquare
-    where
-        nonSquare = filter (not . isSquared) [1 .. 1000]
-        isqrt = floor . sqrt . fromIntegral
-        isSquared x = y * y == x
-            where y = isqrt x
+main = print $ maximumBy (compare `on` (fst . head . diophantine)) nonSquare where
+    nonSquare = filter (not . isSquared) [1 .. 1000]
 
 -- http://en.wikipedia.org/wiki/Pell's_equation
