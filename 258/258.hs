7d9df9146e655f05569b500cb2d6f8ca18290e32
foreverbell <dql.foreverbell@gmail.com>
2015-03-08 09:14:30 +0800

    258: try Vector

--- 258/258.hs
+++ 258/258.hs
@@ -1,35 +1,33 @@
 
 import Control.Monad (forM_, when)
-import Control.Monad.ST
-import Data.Array.Unboxed
-import Data.Array.ST
-import Data.Array.Base (unsafeRead, unsafeWrite)
+import qualified Data.Vector.Unboxed.Mutable as MV
+import qualified Data.Vector.Unboxed as V
+import Data.Vector.Unboxed ((!))
 
 modulo :: Int
 modulo = 20092010
 
-sumMod :: [Int] -> Int
-sumMod xs = foldl helper 0 xs where
-    helper accum x = (accum + x) `mod` modulo
+sumMod :: Int -> Int -> Int
+sumMod a b = (a + b) `mod` modulo
 
-mulMat :: Int -> UArray Int Int -> UArray Int Int -> UArray Int Int
-mulMat k a b = runSTUArray $ do
-    ret <- newArray (0, k - 1) 0
+mulMat :: Int -> V.Vector Int -> V.Vector Int -> V.Vector Int
+mulMat k a b = V.create $ do
+    ret <- MV.replicate k 0
     forM_ [0 .. k - 1] $ \i -> do
         when ((a!i) /= 0) $ do
             forM_ [0 .. k - 1] $ \j -> do
                 let v = (a!i) * (b!j)
                 if (i + j >= k)
-                    then (addArray ret (i + j - k) v) >> (addArray ret (i + j - k + 1) v)
-                    else addArray ret (i + j) v
-    return ret where 
-        addArray a i d = do
-            v <- unsafeRead a i -- v <- readArray a i
-            unsafeWrite a i $ (v + d) `mod` modulo -- writeArray a i $ (v + d) `mod` modulo
+                    then (update ret (i + j - k) v) >> (update ret (i + j - k + 1) v)
+                    else update ret (i + j) v
+    return ret where
+        update vec i d = do
+            v <- MV.unsafeRead vec i
+            MV.unsafeWrite vec i $ sumMod v d 
 
-powMat :: Int -> Int -> UArray Int Int -> UArray Int Int
+powMat :: Int -> Int -> V.Vector Int -> V.Vector Int
 powMat k p x = unit `seq` x `seq` helper k p x unit where
-    unit = listArray (0, k - 1) $ (1 : (replicate (k - 1) 0))
+    unit = V.fromList $ (1 : (replicate (k - 1) 0))
     helper k 0 a r = r
     helper k p a r = a' `seq` r' `seq` helper k (p `div` 2) a' r' where
         a' = mulMat k a a
@@ -38,9 +36,9 @@ powMat k p x = unit `seq` x `seq` helper k p x unit where
                 else r
 
 solve :: Int -> [Int] -> [Int] -> Int -> Int
-solve k a f n = sumMod $ zipWith (*) f (elems power) where
-    b = listArray (0, k - 1) $ reverse a :: UArray Int Int
-    baseMat = listArray (0, k - 1) $ 0 : 1 : (repeat 0)
+solve k a f n = foldl sumMod 0 $ zipWith (*) f (V.toList power) where
+    b = V.fromList $ reverse a 
+    baseMat = V.fromList $ 0 : 1 : (replicate (k - 2) 0)
     power = powMat k n baseMat
     
 main = print $ solve 2000 a f (10^18) where
