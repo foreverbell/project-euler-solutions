e1b9af381219d85b9dcfb4f8c3b25950f7ee9077
foreverbell <dql.foreverbell@gmail.com>
2015-05-05 22:44:14 +0800

    fix

--- 33.hs
+++ 33.hs
@@ -1,6 +1,6 @@
 
 main = print b where
-    magic = [ (as,c) | a <- [1 .. 9], b <- [1 .. 9], c <- [a+1 .. 9], (10*a+b)*c == (10*b+c)*a || (10*b+a)*c == (10*c+b)*a ]
+    magic = [ (a,c) | a <- [1 .. 9], b <- [1 .. 9], c <- [a+1 .. 9], (10*a+b)*c == (10*b+c)*a || (10*b+a)*c == (10*c+b)*a ]
     (numerator, denominator) = unzip magic
     a' = product numerator
     b' = product denominator
