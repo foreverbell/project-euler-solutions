aa7ac809ee312210842576eb17ceb4325241fec8
foreverbell <dql.foreverbell@gmail.com>
2016-02-02 15:14:11 +0800

    185: accelerate

--- src/185.hs
+++ src/185.hs
@@ -2,7 +2,10 @@ import qualified Data.IntMap as M
 import           Control.Monad (guard)
 import           Control.Applicative (empty)
 
--- ~5minutes
+{- real 3m37.442s
+   user 3m36.196s
+   sys  0m1.135s
+-} 
 
 type Input = [(Int, Int)]
 
@@ -13,16 +16,13 @@ type Input = [(Int, Int)]
 encode :: [Int] -> [Int] -> Int
 encode xs cs = foldr f 0 $ zip xs cs
   where
-    f (a, b) v | b > a || b < 0 = -1
+    f (a, b) v | b > a = -1
                | v < 0 = v
                | otherwise = v * (a + 1) + b
 
 decodeBase :: Int -> [Int]
-decodeBase x = helper x 8
-  where
-    helper _ 0 = []
-    helper x k = r : helper q (k - 1)
-      where (q, r) = x `quotRem` 10
+decodeBase x = take 8 $ helper x
+  where helper x = let (q, r) = x `quotRem` 10 in r : helper q
 
 coincide :: [Int] -> [Int] -> Int
 coincide as bs = length $ filter id $ zipWith (==) as bs
@@ -30,29 +30,28 @@ coincide as bs = length $ filter id $ zipWith (==) as bs
 search1 :: Input -> M.IntMap Int
 search1 input = M.fromList $ do
   a <- [0 .. 99999999]
-  let as = decodeBase a
-  let e = encode cs $ map (coincide as) vsd
+  let e = encode cs $ map (coincide (decodeBase a)) vsd
   guard $ e >= 0
   return (e, a)
-  where vs = map ((`quot` 10^8) . fst) input
+  where vs = map ((`rem` 10^8) . fst) input
         cs = map snd input
         vsd = map decodeBase vs
 
 search2 :: Input -> M.IntMap Int -> (Int, Int)
 search2 input m = head $ do
   a <- [0 .. 99999999]
-  let as = decodeBase a
-  let e = encode cs $ zipWith (-) cs $ map (coincide as) vsd
+  let e = encode cs $ map (coincide (decodeBase a)) vsd
   guard $ e >= 0
-  case M.lookup e m of
+  case M.lookup (mv - e) m of
        Just f -> return (f, a)
        Nothing -> empty
-  where vs = map ((`rem` 10^8) . fst) input
+  where vs = map ((`quot` 10^8) . fst) input
         cs = map snd input
         vsd = map decodeBase vs
+        mv = encode cs cs
 
 solve :: Input -> Int
-solve input = a * (10^8) + b
+solve input = b * (10^8) + a
   where (a, b) = search2 input (search1 input)
 
 parse :: String -> Input
